b2e4d2152f993dda54953561b83817db7c5a99df
Xiong Ben <xiongben1993@gmail.com>
2019-10-30 22:40:28 +0800

    [add]

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("hello world!xxx");
+	}
+}
+
+class Person {
+	private String name;
+	private int age;
+	public Person(String name,int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public Person() {
+		
+	}
+}
