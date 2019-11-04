b9fcd82f6e8eb792ed513775bf99d39f5f2d0434
xiongben <xiongben1993@gmail.com>
2019-11-04 16:39:27 +0800

    [add]

--- src/errorLearn/ErrorLearn.java
+++ src/errorLearn/ErrorLearn.java
@@ -1,5 +1,9 @@
 package errorLearn;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.logging.Level;
@@ -13,7 +17,7 @@ public class ErrorLearn {
 //        System.out.println("Today is:66666");
 //    }
 
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) throws Exception {
 //         Logger logger = Logger.getGlobal();
 //         logger.info("start process...");
 //         logger.warning("memory is running out...");
@@ -31,12 +35,48 @@ public class ErrorLearn {
 //          System.out.println(youngClass.getField("name"));
 //          System.out.println(youngClass.getDeclaredField("num"));
 
-          Object M = new Man("xiao ming");
-          Class c = M.getClass();
-          Field f = c.getDeclaredField("name");
-          f.setAccessible(true);
-          Object value = f.get(M);
-          System.out.println(value);
+//          Object M = new Man("xiao ming");
+//          Class c = M.getClass();
+//          Field f = c.getDeclaredField("name");
+//          f.setAccessible(true);
+//          Object value = f.get(M);
+//          System.out.println(value);
+
+//        Man man1 = new Man();
+//        Constructor consl = Integer.class.getConstructor(int.class);
+//        Integer n1 = (Integer) consl.newInstance(123);
+//        System.out.println(Man.class);
+//        System.out.println(man1.getClass());
+
+//        Class i = Integer.class;
+//        Class n = i.getSuperclass();
+//        System.out.println(n);
+//        Class o = n.getSuperclass();
+//        System.out.println(o);
+//        System.out.println(o.getSuperclass());
+
+//        Class s = Integer.class.getSuperclass();
+//        Class[] is = s.getInterfaces();
+//        for (Class i : is) {
+//            System.out.println(i);
+//        }
+
+         InvocationHandler handler = new InvocationHandler() {
+             @Override
+             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                 System.out.println(method);
+                 if(method.getName().equals("morning")) {
+                     System.out.println("Good morning, " + args[0]);
+                 }
+                 return null;
+             }
+         };
+         Hello hello = (Hello) Proxy.newProxyInstance(
+                 Hello.class.getClassLoader(),
+                 new Class[]{Hello.class},
+                 handler
+         );
+         hello.morning("Bob");
     }
 
     static  byte[] toGBK(String s){
@@ -92,4 +132,8 @@ class YoungMan extends Man{
     public YoungMan(){
         super();
     }
+}
+
+interface Hello {
+    void morning(String name);
 }
