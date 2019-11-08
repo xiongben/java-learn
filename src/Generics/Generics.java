072759294fbc2544a5dd9c9b20eb33218940a90a
xiongben <xiongben1993@gmail.com>
2019-11-08 14:17:17 +0800

    [add]

--- src/Generics/Generics.java
+++ src/Generics/Generics.java
@@ -1,13 +1,12 @@
 package Generics;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.Number;
 import java.lang.String;
 import java.lang.Double;
-import java.util.List;
-import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Generics {
     public static void main(String[] args) {
@@ -41,24 +40,82 @@ public class Generics {
 //
 //        System.out.println(p2.getFirst() + ", " + p2.getLast());
 
-          List<String> list = new ArrayList<>();
-          list.add("apple");
-          list.add("pear");
-          for (Iterator<String> it = list.iterator();it.hasNext();){
-              String s = it.next();
-              System.out.println(s);
-          }
+//          List<String> list = new ArrayList<>();
+//          list.add("apple");
+//          list.add("pear");
+//          for (Iterator<String> it = list.iterator();it.hasNext();){
+//              String s = it.next();
+//              System.out.println(s);
+//          }
+//
+//
+//
+//          String[] array = list.toArray(new String[list.size()]);
+//
+//        for (String s:array){
+//            System.out.println(s);
+//        }
+//
+//        Integer[] arr2 = {1,2,3,4,5,6,7};
+//        List<Integer> list2 = Arrays.asList(arr2);
+//
+//        List<Integer> arr3 = Arrays.asList(1,2,3);
+//        System.out.println(arr3.contains(2));
+//        System.out.println(arr3.indexOf(1));
+
+//        List<Person> list1 = Arrays.asList(
+//                new Person("xiaoming",34),
+//                new Person("xiaohong",94),
+//                new Person("xiaoliu",84)
+//        );
+//
+//        System.out.println(list1.contains(new Person("xiaohong",94)));
+
+//         Student s = new Student("xiaoming",99);
+//         Map<String, Student> map1 = new HashMap<>();
+//         map1.put("xiaoming", s);
+//        Student target = map1.get("xiaoming"); // 通过key查找并返回映射的Student实例
+//        System.out.println(target == s); // true，同一个实例
+//        System.out.println(target.score); // 99
+//
+//        Map<String, Integer> map2 = new HashMap<>();
+//        map2.put("apple", 123);
+//        map2.put("pear", 456);
+//        map2.put("banana", 789);
+//        for(String key: map2.keySet()){
+//            Integer value = map2.get(key);
+//            System.out.println(key + " = " + value);
+//        }
+//
+//        Map<String , Integer> map3 = new TreeMap<>();
+//        map3.put("orange",1);
+//        map3.put("apple",2);
+//        map3.put("pear",3);
+//        for(String key: map3.keySet()){
+//            System.out.println(key);
+//        }
+
+//          Queue<String> q = new LinkedList<>();
+//        q.offer("apple");
+//        q.offer("pear");
+//        q.offer("banana");
+//        System.out.println(q.poll()); // apple
+//        System.out.println(q.poll()); // pear
+//        System.out.println(q.poll()); // banana
+//        System.out.println(q.poll()); // null,因为队列是空的
 
 
+//        Queue<User> q = new PriorityQueue<>(new UserComparator());
+//        // 添加3个元素到队列:
+//        q.offer(new User("Bob", "A1"));
+//        q.offer(new User("Alice", "A2"));
+//        q.offer(new User("Boss", "V1"));
+//        System.out.println(q.poll()); // Boss/V1
+//        System.out.println(q.poll()); // Bob/A1
+//        System.out.println(q.poll()); // Alice/A2
+//        System.out.println(q.poll()); // null,因为队列为空
 
-          String[] array = list.toArray(new String[list.size()]);
-
-        for (String s:array){
-            System.out.println(s);
-        }
-
-        Integer[] arr2 = {1,2,3,4,5,6,7};
-        List<Integer> list2 = Arrays.asList(arr2);
+          
 
 }
 
@@ -89,7 +146,52 @@ class Person implements Comparable<Person>{
     public int compareTo(Person other) {
         return this.name.compareTo(other.name);
     }
+    public boolean equals(Object o){  //复写equals方法
+        if(o instanceof Person){
+            Person p = (Person) o;
+            return Objects.equals(this.name,p.name) && this.score == p.score;
+        }
+        return false;
+    }
 }
+
+
+class User {
+    public final String name;
+    public final String number;
+
+    public User(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String toString() {
+        return name + "/" + number;
+    }
+}
+
+class UserComparator implements Comparator<User>{
+    public int compare( User u1, User u2){
+        if(u1.number.charAt(0) == u2.number.charAt(0)){
+            return u1.number.compareTo(u2.number);
+        }
+        if(u1.number.charAt(0) == 'V'){
+            return -1;
+        }else{
+            return 1;
+        }
+    }
+}
+
+class Student {
+    public String name;
+    public int score;
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
 
 //interface Comparable<T> {
 //    /**
