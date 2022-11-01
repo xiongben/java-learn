ef709d98b0796827a46af9e2a8575478cfc08b08
kakaxi <kakaxi@gmail.com>
2022-11-01 23:53:34 +0800

    [add]

--- demo1/src/com/xb/demo1/ATMSystem.java
+++ demo1/src/com/xb/demo1/ATMSystem.java
@@ -1,10 +1,16 @@
 package com.xb.demo1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ATMSystem {
     public static void main(String[] args) {
         ArrayList<Account> accounts = new ArrayList<>();
-        System.out.println("");
+        System.out.println("=====ATM系统初始化=======");
+        System.out.println("1,账户登录");
+        System.out.println("2,账户开户");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请选择你的操作");
     }
 }
