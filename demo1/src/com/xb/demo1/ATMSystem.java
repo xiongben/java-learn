16fdc70b5305039ecbae3043c31dd8f5f7cb83df
kakaxi <kakaxi@gmail.com>
2022-11-02 18:18:00 +0800

    [add]

--- demo1/src/com/xb/demo1/ATMSystem.java
+++ demo1/src/com/xb/demo1/ATMSystem.java
@@ -20,6 +20,7 @@ public class ATMSystem {
             switch (command){
                 case 1:
                     // user login
+                    login(accounts, sc);
                     break;
                 case 2:
                     //user register
@@ -30,6 +31,131 @@ public class ATMSystem {
 
             }
         }
+    }
+
+    /**
+     * account login function
+     * @param accounts
+     * @param sc
+     */
+    private static void login(ArrayList<Account> accounts, Scanner sc) {
+        System.out.println("============Login system===========");
+        if(accounts.size() == 0) {
+            System.out.println("there is no account!");
+            return;
+        }
+        while (true) {
+            System.out.println("Please enter account number:");
+            String cardId = sc.next();
+            Account acc = getAccountByCardId(cardId, accounts);
+            if(acc != null) {
+                while (true) {
+                    System.out.println("Please enter password:");
+                    String pwd = sc.next();
+                    if(acc.getPassWord().equals(pwd)) {
+                        System.out.println("Success!" + acc.getUserName() + " enter the system!");
+                        showUserCommond(sc, acc);
+                        return;
+                    } else {
+                        System.out.println("Your password is error!");
+                    }
+                }
+            } else {
+                System.out.println("your account is not exit!");
+            }
+        }
+    }
+
+    private static void showUserCommond(Scanner sc, Account acc) {
+        while (true) {
+            System.out.println("============ User Main Page ============");
+            System.out.println("1,查询账户");
+            System.out.println("2,存款");
+            System.out.println("3，取款");
+            System.out.println("4，转账");
+            System.out.println("5，修改密码");
+            System.out.println("6，退出");
+            System.out.println("7，注销账户");
+            System.out.println("Choose Operation Type:");
+            int commond = sc.nextInt();
+            switch (commond) {
+                case 1:
+                    showAccount(acc);
+                    break;
+                case 2:
+                    // 存款
+                    depositMoney(acc, sc);
+                    break;
+                case 3:
+                    // 取款
+                    withDrawMoney(acc, sc);
+                    break;
+                case 4:
+                    // 转账
+                    break;
+                case 5:
+                    // 修改密码
+                    break;
+                case 6:
+                    System.out.println("logout!!!");
+                    return;
+                case 7:
+                    break;
+                default:
+                    System.out.println("Operation error!");
+            }
+        }
+    }
+
+    private static void withDrawMoney(Account acc, Scanner sc) {
+        System.out.println("========取钱操作===========");
+        if(acc.getMoney() < 100) {
+            System.out.println("你账户余额不够100，无法取钱！");
+            return;
+        }
+        while (true) {
+            System.out.println("Enter the number of withdraw:");
+            double money = sc.nextDouble();
+
+            if (money > acc.getQuotaMoney()) {
+                System.out.println("超出取款限额" + acc.getQuotaMoney());
+            } else {
+                if (money > acc.getMoney()) {
+                    System.out.println("余额不足！你目前的余额是： " + acc.getMoney());
+                } else {
+                    System.out.println("取款成功！！！");
+                    acc.setMoney(acc.getMoney() - money);
+                    showAccount(acc);
+                    return;
+                }
+            }
+        }
+    }
+
+    /**
+     * 存钱
+     * @param acc
+     * @param sc
+     */
+    private static void depositMoney(Account acc, Scanner sc) {
+        System.out.println("========用户存钱操作=========");
+        System.out.println("enter the money number: ");
+        double money = sc.nextDouble();
+        acc.setMoney(acc.getMoney() + money);
+        System.out.println("success! your Account info: ");
+        showAccount(acc);
+    }
+
+    /**
+     * 展示账户信息
+     * @param acc
+     */
+    private static void showAccount(Account acc) {
+        System.out.println("======== User Account Info Detail ========");
+        System.out.println("Card number: " + acc.getCardId());
+        System.out.println("User name: " + acc.getUserName());
+        System.out.println("Money: " + acc.getMoney());
+        System.out.println("User Limit: " + acc.getQuotaMoney());
     }
 
     /**
