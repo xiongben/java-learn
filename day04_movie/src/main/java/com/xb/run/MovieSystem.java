a0fcf33d22106f0445ddab7fe059508f39822b93
kakaxi <kakaxi@gmail.com>
2022-11-05 17:01:11 +0800

    [add]

--- day04_movie/src/main/java/com/xb/run/MovieSystem.java
+++ day04_movie/src/main/java/com/xb/run/MovieSystem.java
@@ -5,15 +5,16 @@ import com.xb.bean.Customer;
 import com.xb.bean.Movie;
 import com.xb.bean.User;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MovieSystem {
     public static final List<User> ALL_USERS = new ArrayList<>();
 
     public static final Map<Business, List<Movie>> ALL_MOVIES = new HashMap<>();
+
+    public static User loginUser;
+
+    public static final Scanner SYS_SC = new Scanner(System.in);
 
     /**
      3、准备一些测试数据
@@ -67,6 +68,155 @@ public class MovieSystem {
     }
 
     public static void main(String[] args) {
+       showMain();
+    }
 
+    private static void showMain() {
+        System.out.println("==============电影院首页===============");
+        System.out.println("1, 登录");
+        System.out.println("2, 用户注册");
+        System.out.println("3, 商家注册");
+        System.out.println("4, 请输入命令：");
+        while (true) {
+            String command = SYS_SC.nextLine();
+            switch (command) {
+                case "1":
+                    login();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                default:
+                    System.out.println("命令有误，请重新输入！");
+            }
+        }
+    }
+
+    private static void login() {
+        while (true) {
+            System.out.println("=请你输入登录名称：");
+            String loginName = SYS_SC.nextLine();
+            System.out.println("请你输入登录密码：");
+            String password = SYS_SC.nextLine();
+
+            User u = getUserByLoginName(loginName);
+            if(u != null) {
+                if(u.getPassWord().equals(password)) {
+                    loginUser = u;
+                    if(u instanceof Customer) {
+                        //普通用户
+                        showCustomerMain();
+                    } else {
+                        //商户
+                        showBusinessMain();
+                    }
+                    return;
+                } else {
+                    System.out.println("password error!");
+                }
+            } else {
+                System.out.println("user login name error!again!");
+            }
+        }
+    }
+
+    private static void showBusinessMain() {
+        while (true) {
+            System.out.println("============黑马电影商家界面===================");
+            System.out.println(loginUser.getUserName() + (loginUser.getSex()=='男'? "先生":"女士" + "欢迎您进入系统"));
+            System.out.println("1、展示详情:");
+            System.out.println("2、上架电影:");
+            System.out.println("3、下架电影:");
+            System.out.println("4、修改电影:");
+            System.out.println("5、退出:");
+
+            System.out.println("请输入您要操作的命令：");
+            String command = SYS_SC.nextLine();
+            switch (command){
+                case "1":
+                    // 展示全部排片信息
+                    showBusinessInfos();
+                    break;
+                case "2":
+                    // 上架电影信息
+                    addMovie();
+                    break;
+                case "3":
+                    // 下架电影信息
+                    deleteMovie();
+                    break;
+                case "4":
+                    // 修改电影信息
+                    updateMovie();
+                    break;
+                case "5":
+                    System.out.println(loginUser.getUserName() +"请您下次再来啊~~~");
+                    return; // 干掉方法
+                default:
+                    System.out.println("不存在该命令！！");
+                    break;
+            }
+        }
+    }
+
+    private static void updateMovie() {
+    }
+
+    private static void deleteMovie() {
+    }
+
+    private static void addMovie() {
+    }
+
+    private static void showBusinessInfos() {
+
+    }
+
+    private static void showCustomerMain() {
+        while (true) {
+            System.out.println("============黑马电影客户界面===================");
+            System.out.println(loginUser.getUserName() + (loginUser.getSex()=='男'? "先生":"女士" + "欢迎您进入系统" +
+                    "\t余额：" + loginUser.getMoney()));
+            System.out.println("请您选择要操作的功能：");
+            System.out.println("1、展示全部影片信息功能:");
+            System.out.println("2、根据电影名称查询电影信息:");
+            System.out.println("3、评分功能:");
+            System.out.println("4、购票功能:");
+            System.out.println("5、退出系统:");
+            System.out.println("请输入您要操作的命令：");
+            String command = SYS_SC.nextLine();
+//            switch (command){
+//                case "1":
+//                    // 展示全部排片信息
+//                    showAllMovies();
+//                    break;
+//                case "2":
+//                    break;
+//                case "3":
+//                    // 评分功能
+//                    scoreMovie();
+//                    showAllMovies();
+//                    break;
+//                case "4":
+//                    // 购票功能
+//                    buyMovie();
+//                    break;
+//                case "5":
+//                    return; // 干掉方法
+//                default:
+//                    System.out.println("不存在该命令！！");
+//                    break;
+//            }
+        }
+    }
+
+    public static User getUserByLoginName(String loginName) {
+        for(User user : ALL_USERS){
+            if(user.getLoginName().equals(loginName)) {
+                return user;
+            }
+        }
+        return null;
     }
 }
