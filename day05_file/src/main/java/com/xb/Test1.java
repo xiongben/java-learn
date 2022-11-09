b34c459394619b3bb8e55461d1356c18bc965633
kakaxi <kakaxi@gmail.com>
2022-11-09 15:12:35 +0800

    [add]

--- day05_file/src/main/java/com/xb/Test1.java
+++ day05_file/src/main/java/com/xb/Test1.java
@@ -1,7 +1,24 @@
 package com.xb;
 
+import java.io.*;
+
 public class Test1 {
     public static void main(String[] args) {
+        try (
+                InputStream is = new FileInputStream("/Users/admin/Desktop/aa.txt");
+                InputStream bis = new BufferedInputStream(is);
+                OutputStream os = new FileOutputStream("/Users/admin/Desktop/aa2.txt");
+                OutputStream bos = new BufferedOutputStream(os);
+        ) {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = bis.read(buffer)) != -1){
+                bos.write(buffer,0,len);
+            }
+            System.out.println("copy end!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
